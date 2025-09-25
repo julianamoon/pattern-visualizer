f0e7fef15ca28b74dafcdff0dc5b61d54190c5c0
Juliana Moreira <juliana_gillan@hotmail.com>
2025-09-24 21:41:49 -0300

    Eye Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 
+import eyesOpen from "./images/streamline-plump_eye-optic-remix.svg";
+import eyesClosed from "./images/solar_eye-closed-bold.svg";
+
 const buttonStyle =
   "bg-blue-600 text-white py-2 px-2 flex-1 cursor-pointer transition hover:bg-blue-400";
 const activeButtonStyle =
@@ -14,6 +17,7 @@ export default function App() {
   const [repeat, setRepeat] = useState("repeat");
   const [activeSize, setActivSize] = useState("50%");
   const [activeAxis, setActiveAxis] = useState("repeat");
+  const [eyeButton, setEyeButton] = useState(true);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -37,144 +41,150 @@ export default function App() {
       }}
     >
       {/* modal */}
-      <div className="container w-[400px] bg-white px-8 py-6 rounded-lg shadow absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        {/* heading + text */}
-        <h1 className="text-xl font-bold text-center">Pattern Visualizer</h1>
+      <div className="bg-white p-6 rounded-lg shadow absolute top-16 left-1/2 transform -translate-x-1/2">
+        <button
+          className="m-auto block cursor-pointer"
+          onClick={() => setEyeButton(!eyeButton)}
+        >
+          {eyeButton ? (
+            <img src={eyesOpen} alt="" />
+          ) : (
+            <img src={eyesClosed} alt="" />
+          )}
+        </button>
+        {eyeButton && (
+          <div className="w-[400px] pt-2">
+            {/* heading + text */}
+            <h1 className="text-xl font-bold text-center">
+              Pattern Visualizer
+            </h1>
 
-        <p className="text-center text-sm px-4">
-          To use the pattern visualizer please add a image in the input below or
-          add a link to a image with the URL input.
-        </p>
-        {/* form */}
-        <form action="" className="py-4 space-y-2" onSubmit={handleSubmit}>
-          {/* FILE */}
-          <fieldset className="pb-2">
-            <label htmlFor="imageFile" className="font-bold">
-              Add image via file
-            </label>
-            <input
-              type="file"
-              name="imageFile"
-              id="imageFile"
-              accept="image/*"
-              className="block w-full text-sm text-gray-500 file:me-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-blue-600 file:text-white hover:file:bg-blue-400 file:disabled:opacity-50 file:disabled:pointer-events-none"
-              onChange={(e) => setValue(URL.createObjectURL(e.target.files[0]))}
-            />
-          </fieldset>
+            <p className="text-center text-sm px-4">
+              To use the pattern visualizer please add a image in the input
+              below or add a link to a image with the URL input.
+            </p>
+            {/* form */}
+            <form action="" className="py-4 space-y-2" onSubmit={handleSubmit}>
+              {/* FILE */}
+              <fieldset className="pb-2">
+                <label htmlFor="imageFile" className="font-bold">
+                  Add image via file
+                </label>
+                <input
+                  type="file"
+                  name="imageFile"
+                  id="imageFile"
+                  accept="image/*"
+                  className="block w-full text-sm text-gray-500 file:me-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-blue-600 file:text-white hover:file:bg-blue-400 file:disabled:opacity-50 file:disabled:pointer-events-none"
+                  onChange={(e) =>
+                    setValue(URL.createObjectURL(e.target.files[0]))
+                  }
+                />
+              </fieldset>
 
-          {/* URL */}
-          <fieldset>
-            <label htmlFor="imageURL" className="font-bold">
-              Add image via URL
-            </label>
-            <input
-              type="text"
-              name="imageURL"
-              id="imageURL"
-              className="block w-full text-sm border-2 border-blue-600 rounded-lg py-1 px-2 placeholder:text-gray-500"
-              placeholder="Paste URL here"
-              onChange={(e) => setValue(e.target.value)}
-            />
-          </fieldset>
+              {/* URL */}
+              <fieldset>
+                <label htmlFor="imageURL" className="font-bold">
+                  Add image via URL
+                </label>
+                <input
+                  type="text"
+                  name="imageURL"
+                  id="imageURL"
+                  className="block w-full text-sm border-2 border-blue-600 rounded-lg py-1 px-2 placeholder:text-gray-500"
+                  placeholder="Paste URL here"
+                  onChange={(e) => setValue(e.target.value)}
+                />
+              </fieldset>
 
-          {/* SIZE */}
-          <fieldset>
-            <label className="font-bold">Change Size</label>
-            <div className="w-full flex items-stretch justify-stretch">
-              <button
-                data-size="10%"
-                className={`rounded-l-lg
-                                    ${
-                                      activeSize === "10%"
-                                        ? activeButtonStyle
-                                        : buttonStyle
-                                    }`}
-                onClick={(e) => setSize(e.target.dataset.size)}
-              >
-                XS
-              </button>
-              <button
-                data-size="25%"
-                className={
-                  activeSize === "25%" ? activeButtonStyle : buttonStyle
-                }
-                onClick={(e) => setSize(e.target.dataset.size)}
-              >
-                S
-              </button>
-              <button
-                data-size="50%"
-                className={
-                  activeSize === "50%" ? activeButtonStyle : buttonStyle
-                }
-                onClick={(e) => setSize(e.target.dataset.size)}
-              >
-                M
-              </button>
-              <button
-                data-size="100%"
-                className={
-                  activeSize === "100%" ? activeButtonStyle : buttonStyle
-                }
-                onClick={(e) => setSize(e.target.dataset.size)}
-              >
-                G
-              </button>
-              <button
-                data-size="150%"
-                className={`rounded-r-lg
-                                    ${
-                                      activeSize === "150%"
-                                        ? activeButtonStyle
-                                        : buttonStyle
-                                    }`}
-                onClick={(e) => setSize(e.target.dataset.size)}
-              >
-                XG
-              </button>
-            </div>
-          </fieldset>
+              {/* SIZE */}
+              <fieldset>
+                <label className="font-bold">Change Size</label>
+                <div className="w-full flex items-stretch justify-stretch">
+                  <button
+                    data-size="10%"
+                    className={`rounded-l-lg
+                  ${activeSize === "10%" ? activeButtonStyle : buttonStyle}`}
+                    onClick={(e) => setSize(e.target.dataset.size)}
+                  >
+                    XS
+                  </button>
+                  <button
+                    data-size="25%"
+                    className={
+                      activeSize === "25%" ? activeButtonStyle : buttonStyle
+                    }
+                    onClick={(e) => setSize(e.target.dataset.size)}
+                  >
+                    S
+                  </button>
+                  <button
+                    data-size="50%"
+                    className={
+                      activeSize === "50%" ? activeButtonStyle : buttonStyle
+                    }
+                    onClick={(e) => setSize(e.target.dataset.size)}
+                  >
+                    M
+                  </button>
+                  <button
+                    data-size="100%"
+                    className={
+                      activeSize === "100%" ? activeButtonStyle : buttonStyle
+                    }
+                    onClick={(e) => setSize(e.target.dataset.size)}
+                  >
+                    G
+                  </button>
+                  <button
+                    data-size="150%"
+                    className={`rounded-r-lg
+                  ${activeSize === "150%" ? activeButtonStyle : buttonStyle}`}
+                    onClick={(e) => setSize(e.target.dataset.size)}
+                  >
+                    XG
+                  </button>
+                </div>
+              </fieldset>
 
-          {/* AXIS */}
-          <fieldset>
-            <label className="font-bold">Change Axis</label>
-            <div className="w-full flex items-stretch justify-stretch">
-              <button
-                data-size="repeat"
-                className={`rounded-l-lg
-                                    ${
-                                      activeAxis === "repeat"
-                                        ? activeButtonStyle
-                                        : buttonStyle
-                                    }`}
-                onClick={(e) => setRepeat(e.target.dataset.size)}
-              >
-                All Axis
-              </button>
-              <button
-                data-size="repeat-x"
-                className={
-                  activeAxis === "repeat-x" ? activeButtonStyle : buttonStyle
-                }
-                onClick={(e) => setRepeat(e.target.dataset.size)}
-              >
-                X Axis
-              </button>
-              <button
-                data-size="repeat-y"
-                className={`rounded-r-lg
-                                    ${
-                                      activeAxis === "repeat-y"
-                                        ? activeButtonStyle
-                                        : buttonStyle
-                                    }`}
-                onClick={(e) => setRepeat(e.target.dataset.size)}
-              >
-                Y Axis
-              </button>
-            </div>
-          </fieldset>
-        </form>
+              {/* AXIS */}
+              <fieldset>
+                <label className="font-bold">Change Axis</label>
+                <div className="w-full flex items-stretch justify-stretch">
+                  <button
+                    data-size="repeat"
+                    className={`rounded-l-lg
+                  ${activeAxis === "repeat" ? activeButtonStyle : buttonStyle}`}
+                    onClick={(e) => setRepeat(e.target.dataset.size)}
+                  >
+                    All Axis
+                  </button>
+                  <button
+                    data-size="repeat-x"
+                    className={
+                      activeAxis === "repeat-x"
+                        ? activeButtonStyle
+                        : buttonStyle
+                    }
+                    onClick={(e) => setRepeat(e.target.dataset.size)}
+                  >
+                    X Axis
+                  </button>
+                  <button
+                    data-size="repeat-y"
+                    className={`rounded-r-lg
+                  ${
+                    activeAxis === "repeat-y" ? activeButtonStyle : buttonStyle
+                  }`}
+                    onClick={(e) => setRepeat(e.target.dataset.size)}
+                  >
+                    Y Axis
+                  </button>
+                </div>
+              </fieldset>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
